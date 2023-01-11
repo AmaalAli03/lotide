7387994f305c1cc836343f3e7a74854e4f1661de
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-11 14:17:18 -0500

    added how to find the middle element in an array with both odd and even numbers of elements

---
+++ middle.js
@@ -0,0 +1,39 @@
+const eqArrays = function(firstArray, secondArray){
+
+  if(firstArray.length !== secondArray.length){
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i] ) {
+      return false;
+    }
+    
+  }
+  return true;
+}
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==   ${expected}`);
+  }
+};
+const middle = function(middleArray) {
+  //For arrays with one or two elements, there is no middle. Return an empty array.
+  if (middleArray.length <= 2 ) {
+    return [];
+  }if(middleArray.length % 2 == 0){
+//2 elements need to be returned
+return [middleArray[middleArray.length / 2 - 1] , middleArray[middleArray.length / 2] ]
+  }
+  if (middleArray.length % 2 !== 0){
+    const middleNumber =  Math.floor(middleArray.length / 2);
+  return [middleArray[middleNumber]] 
+    
+  }
+  
+}
+console.log(middle([1, 2, 3, 4])) // => [2])
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+//arrays with odd number of elements, an array containing a single middle element should be returned.
+//arrays with an even number of elements, an array containing the two elements in the middle should be returned
