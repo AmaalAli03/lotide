a7e42ffbb922b0664f01a2bb036443aa1e314235
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-16 15:18:30 -0500

    added an assertArraysEqualTest.js file into the Test folder and exported the assertArraysEquals funtion with module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,3 @@
-const eqArrays = function(firstArray, secondArray){
-
-  if(firstArray.length !== secondArray.length){
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i] ) {
-      return false;
-    }
-    
-  }
-  return true;
-}
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
