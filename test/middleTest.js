31ff73d94650c79bad67ec935c51e911e6805157
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-16 18:53:25 -0500

    added changes to the middle, head and tail Test files so that they are mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
 const assertArraysEqual = require('../assertArraysEqual');
 // const eqArrays = require('../eqArrays');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]) // => [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]) // => [3]
+describe("#middle", () => {
+  it("returns [2, 3] for ([1, 2, 3, 4])", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),([2, 3]));
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]);
+  });
+});
+
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]) // => [2])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]) // => [3]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,17 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const test1 = tail([1, 2, 3]);
-assertArraysEqual(test1, [2, 3]);
-console.log(test1);
+describe("#tail", () => {
+  it("returns 2,3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns ['Lighthouse', 'Labs']'for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
 
-const test2 = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(test2, ["Lighthouse", "Labs"]);
-console.log(test2);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-//another way to write^^
-// assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"], ["Lighthouse", "Labs"])) 
-const actual = tail([1, 2, 3]);
-const expected = [2, 3];
-assertArraysEqual(actual, expected);
+});
