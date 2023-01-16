a8e6d492054675814c00f5cc32ea8625e746f19e
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-16 15:28:28 -0500

    added middleTest.js to test folder and exported the middle function

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+const middle = require('../middle');
+
+console.log(middle([1, 2, 3, 4])) // => [2])
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
